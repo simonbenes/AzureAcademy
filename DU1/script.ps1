6513418199534b43bfbf8d2e495353e6ce7ed947
simonbenes <simon.benes@gmail.com>
2019-09-01 02:22:11 +0200

    Create script.ps1

---
+++ DU1/script.ps1
@@ -0,0 +1,27 @@
+<#
+    .SYNOPSIS
+        Skript domaci ulohy 1. Nastavi firewall, nainstaluje IIS
+#>
+
+Param (
+    [string]$user,
+    [string]$password
+)
+
+# Force use of TLS 1.2
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+# Firewall
+netsh advfirewall firewall add rule name="http" dir=in action=block protocol=TCP localport=80
+netsh advfirewall firewall add rule name="https" dir=in action=allow protocol=TCP localport=443
+netsh advfirewall firewall add rule name="rdp" dir=in action=allow protocol=TCP localport=3389
+netsh advfirewall firewall add rule name="test" dir=in action=block protocol=TCP localport=1234
+
+# Install iis
+Install-WindowsFeature web-server -IncludeManagementTools
+
+# Configure iis
+#Remove-WebSite -Name "Default Web Site"
+#Set-ItemProperty IIS:\AppPools\DefaultAppPool\ managedRuntimeVersion ""
+#New-Website -Name "MusicStore" -Port 80 -PhysicalPath C:\music\ -ApplicationPool DefaultAppPool
+#& iisreset
